7f5457225cd5dfd71a05825130d83101419a063e
Mahfujul Islam Ahad <91899388+mahfuz1418@users.noreply.github.com>
2024-10-21 22:15:03 +0600

    Mastaring and login form setup

---
+++ resources/views/livewire/login-component.blade.php
@@ -0,0 +1,72 @@
+<div>
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="index.html" rel="nofollow">Home</a>
+                    <span></span> Login
+                </div>
+            </div>
+        </div>
+        <section class="pt-150 pb-150">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-10 m-auto">
+                        <div class="row">
+                            <div class="col-lg-5">
+                                <div
+                                    class="login_wrap widget-taber-content p-30 background-white border-radius-10 mb-md-5 mb-lg-0 mb-sm-5">
+                                    <div class="padding_eight_all bg-white">
+                                        <div class="heading_s1">
+                                            <h3 class="mb-30">Login</h3>
+                                        </div>
+                                        <form method="POST" action="{{ route('store.login') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <input type="email" id="email" name="email"
+                                                    value="{{ old('email') }}">
+                                                @if ($errors->has('email'))
+                                                    <div class="text-danger mt-2">
+                                                        {{ $errors->first('email') }}
+                                                    </div>
+                                                @endif
+                                            </div>
+
+                                            <div class="form-group">
+                                                <input id="password" type="password" name="password">
+                                                @if ($errors->has('password'))
+                                                    <div class="text-danger mt-2">
+                                                        {{ $errors->first('password') }}
+                                                    </div>
+                                                @endif
+                                            </div>
+                                            <div class="login_footer form-group">
+                                                <div class="chek-form">
+                                                    <div class="custome-checkbox">
+                                                        <input class="form-check-input" type="checkbox" name="checkbox"
+                                                            id="exampleCheckbox1" value="">
+                                                        <label class="form-check-label"
+                                                            for="exampleCheckbox1"><span>Remember me</span></label>
+                                                    </div>
+                                                </div>
+                                                <a class="text-muted" href="#">Forgot password?</a>
+                                            </div>
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-fill-out btn-block hover-up"
+                                                    name="login">Log in</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-1"></div>
+                            <div class="col-lg-6">
+                                <img src="assets/imgs/login.png">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
